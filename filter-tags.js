8e7c651a68840ee0d1f02ddb3a07e8e883fc5d6e
Minh12s <114908270+Minh12s@users.noreply.github.com>
2022-11-18 16:03:10 +0700

    Add files via upload

---
+++ filter-tags.js
@@ -0,0 +1,46 @@
+(function() {
+
+    var $img = $('#gallery img');
+    var $buttons = $('#buttons');
+    var tagged ={};
+
+    $img.each(function() {
+        var img = this;
+        var tags = $(this).data('tags');
+        if (tags) {
+            tags.split(',').forEach(function(tagName) {
+                if (tagged[tagName] == null) {
+                    tagged[tagName] = [];
+                }
+                tagged[tagName].push(img);
+            });
+        }
+    });
+
+    $('<button>', {
+        Text: 'Show All',
+        class: 'active',
+        click: function() {
+            $(this)
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+            $img.show();
+        }
+    }).appendTo($button);
+    $.each(tagged, function(tagName) {
+        $('<button>', {
+            text: tagName + ' (' +tagged[tagName].length + ')',
+            click: function() {
+                $(this)
+                .addClass('active')
+                .siblings()
+                ,removeClass('active');
+                $imgs
+                .hide()
+                .filter(tagged[tagName])
+                .show();
+            }
+        }).appendTo($buttons);
+    });
+}());
